a3552b32f71959d8b66b09725d3c935c222a1d13
Boddu Vamsi <justforaitoolz@gmail.com>
2026-03-01 03:23:56 +0530

    Fix: Use __dirname-relative dotenv path to properly embed API key in packaged builds

--- desktop/src/firebaseConfig.js
+++ desktop/src/firebaseConfig.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: require('path').join(__dirname, '.env') });
 const { initializeApp } = require("firebase/app");
 // For Node.js environment (Electron Main), we use standard firestore
 const { getFirestore } = require("firebase/firestore");
